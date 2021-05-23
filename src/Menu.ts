66aba835440a294b432fe8fd82ab20cde2141062
Gordon <gordonlarrigan@gmail.com>
2021-05-23 21:08:36 +0100

    Start implementing menu system

--- src/Menu.ts
+++ src/Menu.ts
@@ -1,3 +1,18 @@
+import MenuItem from './MenuItem';
+
 export default class Menu {
-  //
+  private items: MenuItem[];
+  private selectedIndex: number;
+
+  constructor(items: MenuItem[]) {
+    this.items = items;
+  }
+
+  update(dt: number): void {
+    //
+  }
+
+  draw(context: CanvasRenderingContext2D): void {
+    //
+  }
 }
